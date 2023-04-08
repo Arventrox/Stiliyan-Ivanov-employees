933e124410b9f81338a16ed61527f26dc5b47a50
Stiliyan Ivanov <stiliqnivanov124@gmail.com>
2023-04-09 01:21:57 +0300

    Working Version

--- README.md
+++ README.md
@@ -1,70 +1,30 @@
-# Getting Started with Create React App
+# Pair of employees who have worked together
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was made with React.
+This project was tested on node v18.11.0.
 
-## Available Scripts
+The data folder is not a part of the project, it is there for convenience.
+The utility functions and all elements of the project are in App.js for your convenience.
 
-In the project directory, you can run:
+## Task
 
-### `npm start`
+Create an application that identifies the pair of employees who have worked
+together on common projects for the longest period of time.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Requirements
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+1. DateTo can be NULL, equivalent to today
+2. The input data must be loaded to the program from a CSV file
+3. Create an UI:
+   The user picks up a file from the file system and, after selecting it, all common
+   projects of the pair are displayed in datagrid with the following columns:
+   Employee ID #1, Employee ID #2, Project ID, Days worked
+4. More than one date format to be supported, extra points will be given if all date formats
+   are supported
 
-### `npm test`
+### Usage
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+1. Run npm start in console
+2. Click **Choose File** to select a csv file, which imports and displays the raw data from the file
+3. Click **Sort Table** to get and display the pair of employees who have worked
+   together on common projects for the longest period of time
--- src/App.js
+++ src/App.js
@@ -1,41 +1,138 @@
 import { useState } from "react";
 import "./App.css";
 import Papa from "papaparse";
+import { getOverlappingDaysInIntervals } from "date-fns";
 
-/* 
-1) DateTo can be NULL, equivalent to today
+//  Function returns array of object values
+const getObjectValues = (input) =>
+  input.map((element) => Object.values(element));
 
-2) The input data must be loaded to the program from a CSV file
+// Function formats the dates to Date datatype
+const formatDates = (input) => {
+  for (const employee of input) {
+    employee.DateFrom = new Date(employee.DateFrom);
+    employee.DateTo =
+      employee.DateTo !== "NULL" ? new Date(employee.DateTo) : new Date();
+  }
 
-3) Create an UI:
+  return input;
+};
 
-The user picks up a file from the file system and, after selecting it, all common
-projects of the pair are displayed in datagrid with the following columns:
-Employee ID #1, Employee ID #2, Project ID, Days worked
+// Function returns groups of employees by their common projects
+const groupByProjectId = (input) => {
+  const arr = input.reduce((group, arr) => {
+    const { ProjectID } = arr;
 
-4) The task solution needs to be uploaded in github.com, repository name must be in
-format: {FirstName}-{LastName}-employees
+    group[ProjectID] = group[ProjectID] ?? [];
+    group[ProjectID].push(arr);
 
-Bonus points
+    return group;
+  }, {});
 
-1) More than one date format to be supported, extra points will be given if all date formats
-are supported
-*/
+  return arr;
+};
 
-function App() {
+// Function returns all the pairs of employees who have worked together on a common project and calculates the overlapping workdays
+const getDaysWorked = (input) => {
+  const arr = groupByProjectId(formatDates(input));
+  const pairsArr = [];
+
+  for (const project in arr) {
+    if (arr[project].length < 2) {
+      delete arr[project];
+    } else {
+      for (const [index, employee1] of arr[project].entries()) {
+        for (const [index2, employee2] of arr[project].entries()) {
+          if (index === index2 || index < index2) continue;
+
+          //Gets the number of days that overlap
+          const days = getOverlappingDaysInIntervals(
+            {
+              start: employee1.DateFrom,
+              end: employee1.DateTo,
+            },
+            {
+              start: employee2.DateFrom,
+              end: employee2.DateTo,
+            }
+          );
+
+          if (days > 0)
+            pairsArr.push({
+              Emp1: employee2.EmpID,
+              Emp2: employee1.EmpID,
+              ProjID: project,
+              DaysWorked: days,
+            });
+        }
+      }
+    }
+  }
+
+  return pairsArr;
+};
+
+// Function returns the combinations of pairs who have worked together for the longest time and sorts them by most days
+const summarizePairsWorkData = (input) => {
+  let arr = [];
+
+  for (const [index, pair] of input.entries()) {
+    let days = 0;
+    let projects = [];
+    arr[index] = {};
+
+    for (const [_, pair2] of input.entries()) {
+      if (pair.Emp1 === pair2.Emp1 && pair.Emp2 === pair2.Emp2) {
+        projects.push(pair2.ProjID);
+        days += pair2.DaysWorked;
+
+        arr[index] = {
+          Emp1: pair.Emp1,
+          Emp2: pair.Emp2,
+          Projects: projects,
+          DaysWorked: days,
+        };
+      }
+    }
+  }
+  const unique = arr.filter(
+    (obj, index) =>
+      arr.findIndex(
+        (item) => item.Emp1 === obj.Emp1 && item.Emp2 === obj.Emp2
+      ) === index
+  );
+  unique.sort((a, b) => b.DaysWorked - a.DaysWorked);
+
+  return unique;
+};
+
+// Function returns the common projects and the workdays of the pair that has worked together for the longest
+const getLongestPairProjects = (input, longestPair) => {
+  const arr = input
+    .filter(
+      (data) => data.Emp1 === longestPair.Emp1 && data.Emp2 === longestPair.Emp2
+    )
+    .sort((a, b) => b.DaysWorked - a.DaysWorked);
+
+  return arr;
+};
+
+const App = () => {
   const [tableName, setTableName] = useState(null);
   const [isTableSorted, setIsTableSorted] = useState(false);
 
-  const [columns, setColumns] = useState([]);
-  const [values, setValues] = useState([]);
-  const [data, setData] = useState();
-  // const [parsedData, setParsedData] = useState([]);
+  const [rawData, setRawData] = useState([]);
+  const [rawColumns, setRawColumns] = useState([]);
+  const [rawValues, setRawValues] = useState([]);
+
+  const [tableData, setTableData] = useState([]);
+  const [combinedDaysWorked, setCombinedDaysWorked] = useState(null);
 
   const fileHandler = (event) => {
     const file = event.target.files[0];
     if (!file) return;
-
     setTableName("Imported Table");
+
     Papa.parse(file, {
       header: true,
       skipEmptyLines: true,
@@ -44,51 +141,43 @@ function App() {
         const columnArr = parsedData.map((element) => Object.keys(element));
         const valuesArr = parsedData.map((element) => Object.values(element));
 
-        setData(parsedData);
-        setColumns(columnArr[0]);
-        setValues(valuesArr);
+        setRawData(parsedData);
+        setRawColumns(columnArr[0]);
+        setRawValues(valuesArr);
       },
     });
   };
+
   const sortTableHandler = () => {
+    const data = [...rawData];
+    if (!data) return;
     setIsTableSorted(true);
     setTableName(
-      "Sorted table by employees who have worked the longest on the same project "
+      "Sorted table by the pair of employees who have worked together on common projects for the longest period of time"
     );
 
-    // group by projects
-    const newArr = [...data];
+    const pairsArr = getDaysWorked(data);
 
-    const groupedArr = newArr.reduce((acc, curr) => {
-      const key = curr.ProjectID;
-      if (!acc[key]) {
-        acc[key] = [];
-      }
+    //Checks if there are pairs of employees
+    if (pairsArr.length === 0) {
+      setCombinedDaysWorked(
+        "There are no employees who have overlapping days of work on the same project"
+      );
+      return;
+    }
 
-      acc[key].push(curr);
+    const summarizedData = summarizePairsWorkData(pairsArr);
+    const longestPairWorkedTogether = summarizedData[0];
+    const longestPairProjects = getLongestPairProjects(
+      pairsArr,
+      longestPairWorkedTogether
+    );
+    const tableDataResult = getObjectValues(longestPairProjects);
 
-      return acc;
-    }, []);
-    // remove singles
-    const filteredArrays = groupedArr.filter((arr) => arr.length > 1);
-
-    //convert dates to DateTime
-
-    const convertedDatesArr = filteredArrays.map((employees) => {
-      employees.map((employee) => {
-        employee.DateFrom !== "NULL" ? new Date(employee.DateFrom) : new Date();
-        employee.DateTo !== "NULL" ? new Date(employee.DateTo) : new Date();
-      });
-      return employees;
-    });
-
-    // sort by start date
-    // convertedDatesArr.sort((a, b) => a.DateFrom - b.DateFrom);
-
-    const sortedArrByStartDate = convertedDatesArr.map((employees) => {
-      employees.sort((a, b) => a.DateFrom - b.DateFrom);
-      return employees;
-    });
+    setCombinedDaysWorked(
+      `This pair has worked together for  ${longestPairWorkedTogether.DaysWorked} days`
+    );
+    setTableData(tableDataResult);
   };
 
   const showOriginalHandler = () => {
@@ -112,13 +201,13 @@ function App() {
           <table>
             <thead>
               <tr>
-                {columns.map((column, index) => (
+                {rawColumns.map((column, index) => (
                   <th key={index}>{column}</th>
                 ))}
               </tr>
             </thead>
             <tbody>
-              {values.map((value, rowIndex) => (
+              {rawValues.map((value, rowIndex) => (
                 <tr key={rowIndex}>
                   {value.map((v, valueIndex) => (
                     <td key={valueIndex}>{v}</td>
@@ -133,23 +222,29 @@ function App() {
               <tr>
                 <th>Employee ID #1</th>
                 <th>Employee ID #2</th>
-                <th>Project ID </th>
+                <th>Project ID</th>
                 <th>Days worked</th>
               </tr>
             </thead>
             <tbody>
+              {tableData.map((value, rowIndex) => (
+                <tr key={rowIndex}>
+                  {value.map((v, valueIndex) => (
+                    <td key={valueIndex}>{v}</td>
+                  ))}
+                </tr>
+              ))}
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td className="tableData-result" colSpan="4">
+                  {combinedDaysWorked}
+                </td>
               </tr>
             </tbody>
           </table>
         )}
 
         <div className="table-operations_container">
-          {!isTableSorted && (
+          {!isTableSorted && tableName && (
             <button onClick={sortTableHandler}>Sort Table</button>
           )}
           {isTableSorted && (
@@ -159,6 +254,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
